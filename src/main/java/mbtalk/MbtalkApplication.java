cd27bed4d1f5053cb9e1b0a39d14bea29925e733
이상욱 <sanguk9907>
2024-01-17 12:11:48 +0900

    first commit

---
+++ src/main/java/mbtalk/MbtalkApplication.java
@@ -0,0 +1,13 @@
+package mbtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtalkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mbtalk/main/controller/MainController.java
@@ -0,0 +1,14 @@
+package mbtalk.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/api/hello")
+    public String test() {
+        System.out.println("?????");
+        return "Hello, world!";
+    }
+
+}
